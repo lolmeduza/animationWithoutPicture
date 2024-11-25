39b728cb71585763aeef6790d6c86bc2bef884b2
Andrey <lolmeduza997@yandex.ru>
2024-11-26 01:03:36 +0300

    qq

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const myImage = new Image();
-myImage.src = "./cyber2.png";
+myImage.src = "./girl3.jpg";
 
 myImage.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
@@ -32,41 +32,49 @@ myImage.addEventListener("load", function () {
     return (
       Math.sqrt(
         red * red * 0.299 + green * green * 0.587 + blue * blue * 0.114
-      ) / 100
+      ) / 150
     );
   }
-
   class Particle {
-    
     constructor() {
-      // this.color = colors[Math.floor(Math.random() * colors.length)];
-      this.x = Math.random() * canvas.width;
-      this.y = 0;
-      this.speed = 10;
-      this.velocity = Math.random() * 0.5;
-      this.size = Math.random() * 1.5 + 1;
-      this.position1 = Math.floor(this.y);
-      this.position2 = Math.floor(this.x);
-    }
-    update() {
-      this.position1 = Math.floor(this.y);
-      this.position2 = Math.floor(this.x);
-      this.speed = mappedImage[this.position1][this.position2][0];
-      let movement = 3.5 - this.speed + this.velocity;
-
-      this.y += movement;
-      if (this.y > canvas.height) {
-        this.y = 0;
         this.x = Math.random() * canvas.width;
-      }
+        this.y = 0;
+        this.speed = 0;
+        this.velocity = Math.random() * 0.5;
+        this.size = Math.random() * 1.5 + 1;
+        this.position1 = Math.floor(this.y);
+        this.position2 = Math.floor(this.x);
+        this.color = "rgb(0, 0, 0)"; // Начальный цвет
     }
+
+    update() {
+        this.position1 = Math.floor(this.y);
+        this.position2 = Math.floor(this.x);
+        this.speed = mappedImage[this.position1][this.position2][0];
+
+        // Перемещение частицы
+        let movement = 2.5 - this.speed + this.velocity;
+        this.y += movement;
+
+        // Если частица выходит за границу экрана, возвращаем её наверх
+        if (this.y > canvas.height) {
+            this.y = 0;
+            this.x = Math.random() * canvas.width;
+        }
+
+        // Обновляем цвет в зависимости от скорости/яркости
+        const brightness = this.speed * 255; // Яркость пикселя
+        this.color = `rgb(${brightness}, ${brightness * 0.5}, ${255 - brightness})`;
+    }
+
     draw() {
-      ctx.beginPath();
-      ctx.fillStyle ="rgb(255, 145, 0)";
-      ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-      ctx.fill();
+        ctx.beginPath();
+        ctx.fillStyle = this.color; // Используем цвет частицы
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
     }
-  }
+}
+
   function init() {
     for (let i = 0; i < numberOfParticles; i++) {
       particlesArray.push(new Particle());
